26465085cc1b1dda9f44dec30295c9e23e3fb70b
chez8990 <chez8990@gmail.com>
2017-07-19 12:56:49 +0800

    preprocessing

--- Kaggle_yummly.py
+++ Kaggle_yummly.py
@@ -2,29 +2,64 @@ import json
 import pandas as pd
 import numpy as np
 import sys
+import re
+from sklearn.preprocessing import LabelEncoder
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-cuisine_id = []
-cusine = []
-ingredients = []
+# cuisine_id = []
+# cuisine = []
+# recipe = []
+# uniqueIng =[]
+# with open('train.json') as f:
+# 	data = json.load(f)
+# 	for i in data:
+# 		cuisine_id.append(i['id'])
+# 		cuisine.append(i['cuisine'])
+# 		ing1 = i['ingredients']
+# 		ing = map(lambda x: re.sub('\(.*\)|[^a-zA-Z\s]','' , x), ing1)
+# 		ing = map(lambda x: x.replace(' ', '_'), ing)
+# 		recipe.append(ing)   
+# 		uniqueIng+= list(filter(lambda x: x not in uniqueIng, ing))
 
-with open('train.json') as f:
-	data = json.load(f)
-	for i in data:
-		cuisine_id.append(i['id'])
-		cusine.append(i['cuisine'])
-		ingredients.append(i['ingredients'])
 
-train = pd.DataFrame({'Cuisine_id':cuisine_id, 'Cuisine': cusine, 'Ingredients': ingredients})
-# train.to_csv('train.csv')
 
-def ifinlist(row, x):
-    if x in row:
-        return True
-    else:
-        return False
 
-print train[train['Ingredients'].apply(lambda x: 'salt' in x)]['Cuisine'].value_counts(sort=False)
-print train[train['Ingredients'].apply(lambda x: 'ground cumin' in x)]['Cuisine'].value_counts(sort=False, dropna= False)
 
+
+
+# IngNumber = LabelEncoder()
+# IngNumber.fit(uniqueIng)
+# toNumber = IngNumber.transform(uniqueIng)
+# ingre_names = IngNumber.classes_
+
+# pre_recipe=recipe
+
+# for rec in recipe:
+# 	rec = IngNumber.transform(rec)
+
+
+# data_save = {'Recipe': pre_recipe, 'RecipeClass': recipe, 'Unqiue ingredients': ingre_names, 'Cuisine': cuisine, 'ClassesNum': toNumber}
+
+
+# np.save('train', data_save)
+
+train = np.load('train.npy')
+
+ingredients = list(train[()]['Unqiue ingredients'])
+
+def common_extraction(ingr):
+	number = ingredients.index(ingr)
+	common = filter(lambda x: ingr in x or x in ingr, ingredients[number:])
+	if common:
+		ingredients.remove(common)
+		return common
+	else:
+		return None
+
+reduction = {}
+for i in ingredients:
+	reduction[i]= common_extraction(i)
+
+print reduction 
+		
